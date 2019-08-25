716cdcb7ac763430120915b51f1b0f3873e5437a
faadilfarick <faadil.farick@outlook.com>
2019-08-25 23:00:00 +0530

    Initial commit

---
+++ Contacts/Controllers/EmployeesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Contacts.Models;
+
+namespace Contacts.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employees employees = db.Employees.Find(id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        
+        }
+        public ActionResult myModal(int Id) //used to get the modal view
+        {
+            ViewBag.type = 1;
+            Employees employees = db.Employees.Find(Id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_myModal", employees);
+
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Position,Office,Age,StartDate")] Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employees);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employees);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employees employees = db.Employees.Find(id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Position,Office,Age,StartDate")] Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employees).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employees);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employees employees = db.Employees.Find(id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employees employees = db.Employees.Find(id);
+            db.Employees.Remove(employees);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Contacts/Models/Employees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Contacts.Models
+{
+    public class Employees
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Position { get; set; }
+        
+        public string Office { get; set; }
+        
+        public int Age { get; set; }
+        
+        public DateTime StartDate { get; set; }
+    }
+}
